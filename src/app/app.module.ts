d796e2d7afee01003b093fed9267b0ceac4d61fc
DGuarch <david@guarch.es>
2018-07-24 14:33:13 +0200

    Vistas de Curso / Modulos / Usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { LogoComponent } from './header/logo/logo.component';
 import { MenuComponent } from './header/menu/menu.component';
-import { PerfilComponent } from './header/perfil/perfil.component';
 import { FooterComponent } from './footer/footer.component';
 import { CopyComponent } from './footer/copy/copy.component';
 import { CursoComponent } from './curso/curso.component';
@@ -26,14 +24,15 @@ import { UserserviceService } from './services/userservice.service';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatRadioModule} from '@angular/material/radio';
 import { DataSource } from '@angular/cdk/table';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoComponent,
     MenuComponent,
-    PerfilComponent,
     FooterComponent,
     CopyComponent,
     CursoComponent,
@@ -45,18 +44,13 @@ import { DataSource } from '@angular/cdk/table';
     UsuarioComponent,
     UsuarionewComponent,
     UsuariodetalleComponent,
-    LoginComponent,
-   
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     routingProv,
     HttpClientModule,
-    BrowserAnimationsModule,
-    MatButtonModule, 
-    MatCheckboxModule,
-    MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
     MatRippleModule,
@@ -64,7 +58,11 @@ import { DataSource } from '@angular/cdk/table';
     MatRadioModule,
     MatSortModule,
     MatTableModule,
-
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatMenuModule,
+    MatIconModule
   ],
 
   exports: [
@@ -76,10 +74,7 @@ import { DataSource } from '@angular/cdk/table';
     MatListModule,
     MatRadioModule,
     MatSortModule,
-    MatTableModule,
-    
-
-    
+    MatTableModule
   ],
   providers: [
   ],
